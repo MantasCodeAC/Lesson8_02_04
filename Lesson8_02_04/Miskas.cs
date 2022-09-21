f09b5178b27bdc153060bfd6f2fb027d6e95d6e2
Mantas Razanta <mantas.razanta@codeacademylt.onmicrosoft.com>
2022-09-21 20:21:22 +0300

    Add project files.

---
+++ Lesson8_02_04/Miskas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8_02_04
+{
+    internal class Miskas
+    {
+        public List<string> Medziai;
+
+        public Miskas(List<string> medziai)
+        {
+            Medziai = medziai;
+        }
+
+        public void PasodintiMedį(string medis)
+        {
+            Medziai.Add(medis);
+        }
+    }
+}
---
+++ Lesson8_02_04/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lesson8_02_04;
+using System.Net.Http.Headers;
+List<string> jaunuolynas = new List<string>();
+Miskas spygliuociai = new Miskas(jaunuolynas);
+spygliuociai.PasodintiMedį("Pušis");
+spygliuociai.PasodintiMedį("Eglė");
+spygliuociai.PasodintiMedį("Kadagys");
+spygliuociai.PasodintiMedį("Kėnis");
+Console.WriteLine("Spygliuočių medžių sąrašas: ");
+for (int i = 0; i < spygliuociai.Medziai.Count; i++)
+{
+    Console.WriteLine(spygliuociai.Medziai[i]);
+}
